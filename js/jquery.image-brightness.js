ab9dce73fb57fc8f2ff3969d4f629312048b0d32
Abhijeet Salunkhe <abhijeet@abhijeet-mac.local>
2016-09-04 11:27:10 +0530

    cleaned trailing whitespace

--- js/jquery.image-brightness.js
+++ js/jquery.image-brightness.js
@@ -1,191 +1,195 @@
 // fallback of Object.create for older browsers
-if(typeof Object.create !== 'function') {
+if (typeof Object.create !== 'function') {
 
-	// defining the same function
-	Object.create = function(obj) {
+    // defining the same function
+    Object.create = function(obj) {
 
-		// calling constructor, cerating object and returning the instance
-		function F() {};
-		F.prototype = obj;
-		return new F();
+        // calling constructor, cerating object and returning the instance
+        function F() {};
+        F.prototype = obj;
+        return new F();
 
-	};
+    };
 
 }
 
 (function($, window, document, undefined) {
 
-	// main logic
-	var Brightness = {
+    // main logic
+    var Brightness = {
 
-		// initialize
-		init: function(options, element, callback) {
-			
-			var self = this;
+        // initialize
+        init: function(options, element, callback) {
 
-			// accept options as object only
-			if(typeof(options) == 'object') {
+            var self = this;
 
-				// check if developer has set the options
-				self.options = $.extend({}, $.fn.imageBrightness.defaults, options);
+            // accept options as object only
+            if (typeof(options) == 'object') {
 
-			} else { self.options = $.fn.imageBrightness.defaults; /* set default options if options set is not an object */ }
+                // check if developer has set the options
+                self.options = $.extend({}, $.fn.imageBrightness.defaults, options);
 
-			// filling required info to the object
-			self.$element 			= $(element);
-			self.imageSource		= self.$element.attr('src');
-			self.imageFileName 		= self.getFilename(false);
-			self.imageDisplayValue 	= 0;
+            } else {
+                self.options = $.fn.imageBrightness.defaults; /* set default options if options set is not an object */
+            }
 
-			// get the image brightness
-			self.calculateBrightness(function(brightness) {
+            // filling required info to the object
+            self.$element = $(element);
+            self.imageSource = self.$element.attr('src');
+            self.imageFileName = self.getFilename(false);
+            self.imageDisplayValue = 0;
 
-				// deleting the reference image
-				$('img.jQuery-image-brightness-delete-me').remove();
+            // get the image brightness
+            self.calculateBrightness(function(brightness) {
 
-				self.imageDisplayValue = self.getDisplayValue(brightness);
-				callback(self);
-				//console.log('Image "' + self.imageFileName + '" is: ' + self.imageDisplayValue);
+                // deleting the reference image
+                $('img.jQuery-image-brightness-delete-me').remove();
 
-			});
+                self.imageDisplayValue = self.getDisplayValue(brightness);
+                callback(self);
+                //console.log('Image "' + self.imageFileName + '" is: ' + self.imageDisplayValue);
 
-		},
+            });
 
-		// get the display value
-		getDisplayValue: function(brightness) {
+        },
 
-			var self = this;
+        // get the display value
+        getDisplayValue: function(brightness) {
 
-			// check if asked for brightness or opposite value
-			if(self.options.reverseValue) { // if asked for reverse value
-				
-				displayValue = 255 - brightness;
+            var self = this;
 
-			} else { displayValue = brightness; }
+            // check if asked for brightness or opposite value
+            if (self.options.reverseValue) { // if asked for reverse value
 
-			return displayValue;
+                displayValue = 255 - brightness;
 
-		},
+            } else {
+                displayValue = brightness;
+            }
 
-		// get image file name
-		getFilename: function(extension) {
+            return displayValue;
 
-			var self = this;
+        },
 
-			//var filename = self.imageSource.replace(/^.*[\\\/]/, '');
+        // get image file name
+        getFilename: function(extension) {
 
-			var filename = self.imageSource.replace(/\\/g, '/');
-			filename 	 = filename.substring(filename.lastIndexOf('/')+ 1);
-			filename 	 = extension ? filename.replace(/[?#].+$/, ''): filename.split('.')[0];
+            var self = this;
 
-			return filename;
+            //var filename = self.imageSource.replace(/^.*[\\\/]/, '');
 
-		},
+            var filename = self.imageSource.replace(/\\/g, '/');
+            filename = filename.substring(filename.lastIndexOf('/') + 1);
+            filename = extension ? filename.replace(/[?#].+$/, '') : filename.split('.')[0];
 
-		// function calculates and returns the brightness of an image
-		calculateBrightness: function(callback) { // return value ranges between 0 and 255
+            return filename;
 
-			var self = this;
+        },
 
-			// creating alias of the image for reference
-			var img = document.createElement('img');
+        // function calculates and returns the brightness of an image
+        calculateBrightness: function(callback) { // return value ranges between 0 and 255
 
-			// append reference image to the body
-			$(img).attr({
+            var self = this;
 
-				'src': self.imageSource,
-				'class': 'jQuery-image-brightness-delete-me' // need a handle to delete the tmp image after process finished
+            // creating alias of the image for reference
+            var img = document.createElement('img');
 
-			}).hide();
+            // append reference image to the body
+            $(img).attr({
 
-			document.body.appendChild(img);
+                'src': self.imageSource,
+                'class': 'jQuery-image-brightness-delete-me' // need a handle to delete the tmp image after process finished
 
-			// init var to store sum of the rgb for each pixel
-			var colorSum = 0;
+            }).hide();
 
-			// on image load 
-			img.onload = function() {
+            document.body.appendChild(img);
 
-				//console.log('Image: '+self.imageFileName+' is exist!');
+            // init var to store sum of the rgb for each pixel
+            var colorSum = 0;
 
-				// create canvas and draw image
-				var canvas = document.createElement('canvas');
+            // on image load 
+            img.onload = function() {
 
-				// setting up image dimentions to the canvas
-				canvas.width  = this.width;
-				canvas.height = this.height;
+                //console.log('Image: '+self.imageFileName+' is exist!');
 
-				var ctx = canvas.getContext('2d');
+                // create canvas and draw image
+                var canvas = document.createElement('canvas');
 
-				// drawing image on canvas with position top left
-				ctx.drawImage(this, 0, 0);
+                // setting up image dimentions to the canvas
+                canvas.width = this.width;
+                canvas.height = this.height;
 
-				// get image data from top left to bottom right
-				var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-				var data 	  = imageData.data;
-				var r, g, b, avg;
+                var ctx = canvas.getContext('2d');
 
-				// reading rgb values
-				for(var x = 0, len = data.length; x < len; x += 4) {
-					
-					r = data[x];
-					g = data[x + 1];
-					b = data[x + 2];
+                // drawing image on canvas with position top left
+                ctx.drawImage(this, 0, 0);
 
-					// round of the number to avoid decimal values
-					avg       = Math.floor((r + g + b) / 3);
-					colorSum += avg;
+                // get image data from top left to bottom right
+                var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                var data = imageData.data;
+                var r, g, b, avg;
 
-				}
+                // reading rgb values
+                for (var x = 0, len = data.length; x < len; x += 4) {
 
-				// calculating an average
-				var brightness = Math.floor(colorSum / (this.width * this.height));
+                    r = data[x];
+                    g = data[x + 1];
+                    b = data[x + 2];
 
-				callback(brightness); // done reading the image file
+                    // round of the number to avoid decimal values
+                    avg = Math.floor((r + g + b) / 3);
+                    colorSum += avg;
 
-			}
+                }
 
-			img.onerror = function() {
+                // calculating an average
+                var brightness = Math.floor(colorSum / (this.width * this.height));
 
-				//console.log('Image: '+self.imageFileName+' doesn\'t exist!');
+                callback(brightness); // done reading the image file
 
-			}
+            }
 
-		}
+            img.onerror = function() {
 
-	};
+                //console.log('Image: '+self.imageFileName+' doesn\'t exist!');
 
-	// main function declaration
-	$.fn.imageBrightness = function(options, onComplete) {
+            }
 
-		// loop through all elements
-		this.each(function() {
+        }
 
-			// creating an instance
-			var brightness = Object.create(Brightness);
+    };
 
-			// initialize the process
-			brightness.init(options, this, function(obj) {
+    // main function declaration
+    $.fn.imageBrightness = function(options, onComplete) {
 
-				// make sure the callback is a function
-				if(typeof onComplete == 'function') {
-					onComplete(obj.$element, obj.imageDisplayValue); // brings the scope to the callback
-				}
+        // loop through all elements
+        this.each(function() {
 
-			});
+            // creating an instance
+            var brightness = Object.create(Brightness);
 
-		});
+            // initialize the process
+            brightness.init(options, this, function(obj) {
 
-		// return the object to continue chaining
-		return this;
+                // make sure the callback is a function
+                if (typeof onComplete == 'function') {
+                    onComplete(obj.$element, obj.imageDisplayValue);
+                }
 
-	};
+            });
 
-	// keeping the options beyond the scope of the function to make this available for the developer directly
-	$.fn.imageBrightness.defaults = {
+        });
 
-		reverseValue: false, // to get contrast value
+        // return the object to continue chaining
+        return this;
 
-	};
+    };
 
-}) (jQuery, window, document);
+    // keeping the options beyond the scope of the function to make this available for the developer directly
+    $.fn.imageBrightness.defaults = {
+
+        reverseValue: false, // to get contrast value
+
+    };
+
+})(jQuery, window, document);
